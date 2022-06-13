64aa26cd605da931913a902d6a51cfb5e9937350
pino1472 <hanaserebu1472@gmail.com>
2022-06-14 06:17:57 +0900

    少し編集

--- discordbot.py
+++ discordbot.py
@@ -8,30 +8,31 @@ import concurrent.futures
 bot = commands.Bot(command_prefix='/')
 NEWS_CHANNEL_ID = ''
 
-# メッセージ受信時に動作する処理
-@bot.command()
-async def setup(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # /setupで発言チャンネルをセット
-    NEWS_CHANNEL_ID = message.channel
-    looper.start()
+class MyCog(commands.Cog):
+    # メッセージ受信時に動作する処理
+    @bot.command()
+    async def setup(message):
+        # メッセージ送信者がBotだった場合は無視する
+        if message.author.bot:
+            return
+        # /setupで発言チャンネルをセット
+        NEWS_CHANNEL_ID = message.channel
+        looper.start()
 
-@tasks.loop(seconds=60)
-async def looper():
+    @tasks.loop(seconds=60)
+    async def looper():
     
-        channel = client.get_channel(NEWS_CHANNEL_ID) #発言チャンネルを指定
-        news_list = rss_picker() #ニュースを取得
+            channel = bot.get_channel(NEWS_CHANNEL_ID) #発言チャンネルを指定
+            news_list = rss_picker() #ニュースを取得
 
-        #ニュースをチャットに送信
-        for news in news_list:
-            await channel.send(news)
+            #ニュースをチャットに送信
+            for news in news_list:
+                await channel.send(news)
 
-@looper.before_loop
-async def on_ready():
-    # 起動したらログイン通知が表示される
-    await NEWS_CHANNEL_ID.send('開始しました。')
+    @looper.before_loop
+    async def on_ready():
+        # 起動したらログイン通知が表示される
+        await NEWS_CHANNEL_ID.send('開始しました。')
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
