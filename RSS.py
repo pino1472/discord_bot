c1e8ce379606fbe283b9f490adc18eb14b32cdc7
pino1472 <hanaserebu1472@gmail.com>
2022-06-14 04:17:06 +0900

    temp

--- RSS.py
+++ RSS.py
@@ -1,31 +1,54 @@
-RSS_URL = 'https://www.happyou.info/fs/gen.php?u=-1032701763&p=139401332' #AUTOMATONのRSS-URL
+import feedparser
+import git
 
+# urlは適宜自身が編集可能なレポジトリに書き換えてください
+url = 'https://github.com/pino1472/discord_bot.git'
+
+# cloneしたプロジェクトを出力するパス
+to_path = 'data'
 log_path = 'data/log.txt' #logファイルのパス
 
-no_news_flg = 1 #最新ニュース有無のフラグ
+RSS_URL = 'https://www.happyou.info/fs/gen.php?u=-1032701763&p=139401332' #AUTOMATONのRSS-URL
 
-#受け渡しニュースデータ用
-news_list = []
+def rss_picker():
 
-#logから前回取得した最新のニュースURLを取得
-with open(log_path, mode='r') as f:
-    old_news_url = f.read()
-    print('前回取得したニュースURL: '+ old_news_url)
+    no_news_flg = 1 #最新ニュース有無のフラグ
 
-#最新のニュースを取得
-d = feedparser.parse(RSS_URL)
-for entry in d.entries:
+    #受け渡しニュースデータ用
+    news_list = []
 
-    #前回の最新ニュースに当たれば処理終了 (それ以降は古いニュースの為)
-    if old_news_url == entry.link:
-        print('最新のニュースは以上です')
-        break
+    git.Repo.clone_from(
+    url,
+    to_path)
 
-if no_news_flg == 0:
-    #最新のニュースURLに置き換える
-    with open(log_path, mode='w') as f:
-        f.write(d.entries[0].link)
+    #logから前回取得した最新のニュースURLを取得
+    with open(log_path, mode='r') as f:
+        old_news_url = f.read()
+        print('前回取得したニュースURL: '+ old_news_url)
 
-else:
-    #ニュースが無いデータを格納
-    news_list.append('最新のゲーム無料配布ニュースはありません')
+    #最新のニュースを取得
+    d = feedparser.parse(RSS_URL)
+    for entry in d.entries:
+
+       #前回の最新ニュースに当たれば処理終了 (それ以降は古いニュースの為)
+       if old_news_url == entry.link:
+           news_list.append(entry.link)
+           no_news_flg = 0
+           break
+       else:
+           #ニュースが無いデータを格納
+           news_list.append('最新のニュースはありません')
+    if no_news_flg == 0:
+           #最新のニュースURLに置き換える
+           try:
+               with open(log_path, mode='w') as f:
+               f.write(d.entries[0].link)
+
+               repo.git.add('log.txt')
+               repo.git.commit('log.txt', message='update', author='pino1472')
+               repo.git.push('origin', 'main')
+               repo.git.push('--delete', 'origin', 'main')
+           except IndexError:
+               print('正常にニュースを取得できませんでした。')
+               print('次のサイトのRSSフィード等を再度確認してください: '+ site_name)
+    return news_list
